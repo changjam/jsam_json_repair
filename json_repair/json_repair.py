379a025b9c3003f39408f6b1ca3dcd35e7427a25
張家誠 <changjam60@gmail.com>
2024-07-10 22:18:47 +0800

    feat: add basic template

---
+++ json_repair/json_repair.py
@@ -0,0 +1,19 @@
+import json
+
+class json_parser:
+    def __init__(self):
+        pass
+
+    def repair(self, json_string: str) -> dict:
+        try:
+            return json.loads(json_string)
+        except:
+            return None
+
+
+def repair_json(input_string: str) -> dict:
+    json_parser_instance = json_parser()
+    result_json: dict | None = json_parser_instance.repair(input_string)
+    if result_json is None:
+        raise ValueError("Invalid JSON string")
+    return result_json
