d55e42971b94baa41c9dec091fa65d329d0cbd01
張家誠 <changjam60@gmail.com>
2024-07-12 21:39:09 +0800

    feat: add test case

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import unittest
 from json_repair import repair_json
+from error_format import *
 
 
 
@@ -7,6 +8,17 @@ from json_repair import repair_json
 class TestJsonRepair(unittest.TestCase):
     def test_basic_types_valid(self):
         self.assertEqual(repair_json('{"test": 123}'), {"test": 123})
+    def test_lack_of_comma(self):
+        self.assertEqual(repair_json(ERROR_FORMAT1), CORRECT_FORMAT1)
+    def test_redundant_comma_at_the_end(self):
+        self.assertEqual(repair_json(ERROR_FORMAT2), CORRECT_FORMAT2)
+    def test_missing_quotation_marks(self):
+        self.assertEqual(repair_json(ERROR_FORMAT3), CORRECT_FORMAT3)
+    def test_missing_parentheses(self):
+        self.assertEqual(repair_json(ERROR_FORMAT4), CORRECT_FORMAT4)
+    def test_error_boolean_value(self):
+        print(repair_json(ERROR_FORMAT5))
+        self.assertEqual(repair_json(ERROR_FORMAT5), CORRECT_FORMAT5)
 
 
 class TestExceptions(unittest.TestCase):
